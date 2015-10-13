42bdacaaa4680259fdfadcd4c1737e7db6d83600
Ken Wakita <wakita@is.titech.ac.jp>
2015-10-13 11:04:20 +0900

    第二回義資料とサンプルコード

--- test/lx02a-leapyear.scala
+++ test/lx02a-leapyear.scala
@@ -9,17 +9,4 @@ class LX02ATest extends FlatSpec with Matchers {
     leapyear(2004) should be (true)
     leapyear(2008) should be (true)
   }
-
-  "leapyear" should "be false for 4で割り切れない年" in {
-    leapyear(2001) should be (false)
-    leapyear(2002) should be (false)
-    leapyear(2003) should be (false)
-  }
-
-  "leapyear" should "be false to 100で割り切れる年" in {
-    leapyear(1800) should be (false)
-    leapyear(1900) should be (false)
-    leapyear(2000) should be (false)
-  }
 }
-
